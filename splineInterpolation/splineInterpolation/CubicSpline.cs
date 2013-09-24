148a3b80fe302abb127f2513f1f2e8bdc1f330c8
Ilyazykov <ilya.zykov52@gmail.com>
2013-09-25 01:16:36 +0400

    Test Result os Spline Added

--- splineInterpolation/splineInterpolation/CubicSpline.cs
+++ splineInterpolation/splineInterpolation/CubicSpline.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace splineInterpolation
 {
     public class CubicSpline
     {
+        private List<Polynomial> splineTurple;
 
+        public CubicSpline()
+        {
+            //TODO
+        }
+
+        public CubicSpline(List<Array> x)
+        {
+            // TODO
+        }
+
+        public double getValue(double x)
+        {
+			// TODO
+            return 0;
+        }
     }
 }
--- splineInterpolation/splineInterpolationTest/CubicSplineTest.cs
+++ splineInterpolation/splineInterpolationTest/CubicSplineTest.cs
@@ -1,6 +1,6 @@
-﻿using splineInterpolation;
+﻿using System;
+using splineInterpolation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace splineInterpolationTest
 {
@@ -8,8 +8,17 @@ namespace splineInterpolationTest
     public class CubicSplineTest
     {
         [TestMethod]
-        public void CubicSplineConstructorTest()
+        public void CubicSpline_GetResult()
         {
+            // arrange
+            CubicSpline spline = new CubicSpline();
+            double expected = Math.Sin(0.5);
+
+            // act
+            double actual = spline.getValue(0.5);
+
+            // assert
+            Assert.AreEqual(expected, actual, 0.1, "The value of spline and sin is too different");
         }
     }
 }
